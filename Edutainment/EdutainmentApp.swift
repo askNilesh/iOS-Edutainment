6bae1bb455faf4a790404bf756a4a74a52de25bc
AskNilesh <rathodnilsrk@gmail.com>
2023-05-03 20:58:34 +0530

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Nilesh Rathod on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
